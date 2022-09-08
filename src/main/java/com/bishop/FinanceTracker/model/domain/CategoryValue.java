b9ccbf14409cba789fd9b92e5ce8275950083e4a
bisho <bishoptim453@gmail.com>
2022-09-09 08:50:24 +1000

    - Truncate double values

--- src/main/java/com/bishop/FinanceTracker/model/domain/CategoryValue.java
+++ src/main/java/com/bishop/FinanceTracker/model/domain/CategoryValue.java
@@ -7,15 +7,17 @@ public class CategoryValue {
 
     private String category;
     private String month;
-    private Integer value;
+    private Double value;
+    private Integer intValue;
 
     public CategoryValue(String category, String month) {
         this.category = category;
         this.month = month;
-        this.value = 0;
+        this.value = 0.0;
     }
 
     public void incrementValue(Double value) {
-        this.value += value.intValue();
+        this.value += value;
+        this.intValue = value.intValue();
     }
 }
